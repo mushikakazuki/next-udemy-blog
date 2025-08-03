9d377f3202ee889e6af5050ddd1767325f7bf8c0
mushikakazuki <51257261+mushikakazuki@users.noreply.github.com>
2025-08-03 16:56:37 +0900

    supabaseの設定

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,16 +1,37 @@
+import { supabase } from "@/lib/supabase";
 import { writeFile } from "fs/promises";
 import path from "path";
 
 export async function saveImage(file: File): Promise<string | null> {
-    const buffer = Buffer.from(await file.arrayBuffer())
-    const fileName = `${Date.now()}_${file.name}`
-    const uploadDir = path.join(process.cwd(), 'public/images')
+    const useSupabase = process.env.NEXT_PUBLIC_USE_SUPABASE_STORAGE=== 'true';
 
-    try {
-        const filePath = path.join(uploadDir, fileName)
-        await writeFile(filePath, buffer);
-        return `/images/${fileName}`
-    } catch {
-        return null
+    if(useSupabase) {
+        const fileName = `${Date.now()}_${file.name}`;
+        const { error } = await supabase.storage
+            .from('udemy-blog')
+            .upload(fileName, file, {
+                cacheControl: '3600',
+                upsert: false,
+        });
+        
+        if (error) {
+            console.error('Upload error:', error.message);
+            return null;
+        }
+        
+        const { data: publicUrlData } = supabase.storage.from('udemy-blog').getPublicUrl(fileName);
+        return publicUrlData.publicUrl;
+    } else {
+        const buffer = Buffer.from(await file.arrayBuffer())
+        const fileName = `${Date.now()}_${file.name}`
+        const uploadDir = path.join(process.cwd(), 'public/images')
+    
+        try {
+            const filePath = path.join(uploadDir, fileName)
+            await writeFile(filePath, buffer);
+            return `/images/${fileName}`
+        } catch {
+            return null
+        }
     }
 }
